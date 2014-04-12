10c5ee5063bf349f17eda1e4547ad4d0436aef96
sashalives <seheinen@gmail.com>
2014-04-11 21:03:22 -0700

    first commit

---
+++ soundgeocache/soundgeocache/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  soundgeocache
+//
+//  Created by Sasha Heinen on 4/11/14.
+//  Copyright (c) 2014 Sasha Heinen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ soundgeocache/soundgeocache/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  soundgeocache
+//
+//  Created by Sasha Heinen on 4/11/14.
+//  Copyright (c) 2014 Sasha Heinen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
